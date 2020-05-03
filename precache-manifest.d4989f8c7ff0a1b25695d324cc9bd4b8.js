5d2df3f8daa6d11e49c84066665d7185b6be63d9
DorKatzir <DorKatzir@users.noreply.github.com>
2020-05-03 13:13:23 +0300

    Updates

---
+++ precache-manifest.d4989f8c7ff0a1b25695d324cc9bd4b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2bbcfd11c33d5b492028cb70a3edfc",
+    "url": "/robofriends-app/index.html"
+  },
+  {
+    "revision": "e687364f759d6c440f13",
+    "url": "/robofriends-app/static/css/main.1ee040aa.chunk.css"
+  },
+  {
+    "revision": "3317b26c688542895502",
+    "url": "/robofriends-app/static/js/2.dfad0a7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-app/static/js/2.dfad0a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e687364f759d6c440f13",
+    "url": "/robofriends-app/static/js/main.d807e6af.chunk.js"
+  },
+  {
+    "revision": "a4b4b1aff6865d8e5a0f",
+    "url": "/robofriends-app/static/js/runtime-main.5163164d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robofriends-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
